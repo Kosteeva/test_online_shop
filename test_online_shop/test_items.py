670bf9c44a01c574735d9f902bad4714d5a75437
Kosteeva <113518011+Kosteeva@users.noreply.github.com>
2022-11-20 16:27:10 +0300

    Update test_items.py
    add changes

--- test_online_shop/test_items.py
+++ test_online_shop/test_items.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_add_to_basket(browser):
     browser.get(link)
